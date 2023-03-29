61de6f2810730bb8a6868d4467638b88a2d59115
Nicola Lambiase <nicola.lambiase@rohde-schwarz.com>
2023-03-29 08:54:24 +0200

    Add ability to send test docstrings

---
+++ tests/test_docstrings.py
@@ -0,0 +1,61 @@
+TEST_DOCSTRING = "This test will always pass."
+
+
+def test_add_docstrings(run_mocked_pytest, session_uuid):
+    runpytest, sender = run_mocked_pytest
+    result = runpytest(
+        f"--session-uuid={session_uuid}",
+        "--add-docstrings",
+        pyfile=f"""
+    def test_base():
+        '''
+        {TEST_DOCSTRING}
+        '''
+        assert True
+    """,
+    )
+    fluent_sender = sender.return_value
+    call_args = fluent_sender.emit_with_time.call_args_list
+    result.assert_outcomes(passed=1)
+    assert len(call_args) > 0
+    report = call_args[2].args[2]
+    assert "docstring" in report
+    assert report.get("docstring") == TEST_DOCSTRING
+
+
+def test_docstrings_disabled(run_mocked_pytest, session_uuid):
+    runpytest, sender = run_mocked_pytest
+    result = runpytest(
+        f"--session-uuid={session_uuid}",
+        pyfile=f"""
+    def test_base():
+        '''
+        {TEST_DOCSTRING}
+        '''
+        assert True
+    """,
+    )
+    fluent_sender = sender.return_value
+    call_args = fluent_sender.emit_with_time.call_args_list
+    result.assert_outcomes(passed=1)
+    assert len(call_args) > 0
+    report = call_args[2].args[2]
+    assert "docstring" not in report
+
+
+def test_missing_docstring(run_mocked_pytest, session_uuid):
+    runpytest, sender = run_mocked_pytest
+    result = runpytest(
+        f"--session-uuid={session_uuid}",
+        "--add-docstrings",
+        pyfile="""
+    def test_base():
+        assert True
+    """,
+    )
+    fluent_sender = sender.return_value
+    call_args = fluent_sender.emit_with_time.call_args_list
+    result.assert_outcomes(passed=1)
+    assert len(call_args) > 0
+    report = call_args[2].args[2]
+    assert "docstring" not in report
